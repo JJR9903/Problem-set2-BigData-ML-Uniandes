58dd80b724f04f23f44392db39bf576729358692
juanitachaconl <juanitach99@hotmail.com>
2022-09-29 19:47:18 -0500

    Update matrix.R

--- scripts/matrix.R
+++ scripts/matrix.R
@@ -5,6 +5,12 @@ train<-model.matrix(object = ~ Ingtotugarr+JH_Edad2+JH_Edad+Depto + Hombres + Mu
 ##formula 
 model_Ing<-formula(Ingtotugarr~JH_Edad2+JH_Edad+Depto + Hombres + Mujeres+
                      SSalud+Clase+ P_o + JH_RSS_S + P5100+ P5090+CotizaPension)
+## Entrenamiento del modelo 
+
+train<-model.matrix(object = ~ Ingtotugarr JH_Edad2+JH_Edad+Depto + Hombres + Mujeres+
+                      SSalud+Clase+ P_o + JH_RSS_S + P5100+ P5090+CotizaPension)
+
+Train_y<-data.frame(train[,c('Lp','Pobre')],rowIndex=as.numeric(rownames(train)))
 
 ##dento de las recomendaciones ta poner una interacción con seguridad social y la interacción de Clase 
 ##con depto pero no se si valgala pena hacela, revisemos sin eso primero 
